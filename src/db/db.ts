efb47811368dd290b39f445c9e4301e976cc2c3e
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-07-27 15:16:50 +0300

    updated db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,5 @@
 import knex, { Knex } from "knex";
+import { DBExc } from "../common/exception";
 import config from "../config";
 
 class DB {  
@@ -12,20 +13,36 @@ class DB {
             port: config.PG_PORT
         }
     }
+
+    mig_config: Knex.MigratorConfig = {
+        directory: "./src/db/migrations",
+        tableName: "knex_migrations"
+    } //?
     
     knx: Knex = knex(this.config);
+
     async start(): Promise<boolean> {
         return new Promise<boolean>(async (resolve, reject) => {
-            console.log(`db connected on port ${config.PG_PORT} at ${config.PG_HOST} in ${config.STAT} mode`);
 
-            await this.knx.raw('SELECT now()')
-            .catch((err) => {
-                console.log(err);
-                reject(new Error('Unable to connect to Postgres via Knex. Ensure a valid connection.'));
+            this.knx.migrate.latest(this.mig_config).then(async (res) => {
+                console.log(res);
+                console.log("migration completed successfuly");
+
+                await this.knx.raw('SELECT now()')
+                .catch((err) => {
+                    console.log(err);
+                    reject(new Error('Unable to connect to Postgres via Knex. Ensure a valid connection.'));
+                })
+                .finally(() => {
+                    console.log(`db connected on port ${config.PG_PORT} at ${config.PG_HOST} in ${config.STAT} mode`);
+                    resolve(true);
+                });
             })
-            .finally(() => {
-                resolve(true);
-            });
+            .catch((err) => {
+                const result = this.knx.migrate.rollback();
+                console.log(result);
+                reject(new DBExc(err));
+            });         
         });
     }
 }
