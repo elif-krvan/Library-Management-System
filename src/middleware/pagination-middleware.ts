8e2777fda5b7d3770bd6855217606749cecc1086
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-07-29 20:23:15 +0300

    updated pagination middleware

--- src/middleware/pagination-middleware.ts
+++ src/middleware/pagination-middleware.ts
@@ -5,37 +5,31 @@ import { Pagination } from '../interface/i_pagination';
 import user_validation from '../validation/user-validation';
 
 function find_offset(limit: number, page: number): number {
-    return (page - 1) * limit;
+    return (page) * limit; //page index starts from 0 //test
 }
 
 function pagination_middleware(req: ReqPagination, res: Response, next: NextFunction) {
     let pag = {
-        page: req.query.page as string,
-        limit: req.query.limit as string,
+        page: req.query.page as string | number,
+        limit: req.query.limit as string | number,
         sort_by: req.query.sort_by as string,
         order: req.query.order as string
     };
 
-    user_validation.pag_schema.validateAsync((pag)).then((validated_pag: Pagination) => {
-        validated_pag.sort_by = validated_pag.sort_by == undefined ? "id" : validated_pag.sort_by;
-        validated_pag.order = validated_pag.order == undefined ? "asc" : validated_pag.order;
-        
-        validated_pag.limit = validated_pag.limit == undefined ? 15 : (validated_pag.limit > 500 ? 500 : validated_pag.limit);
-        validated_pag.page = validated_pag.page == undefined ? 1 : validated_pag.page; // first page 1 or 0?
+    pag.sort_by = pag.sort_by == undefined ? "id" : pag.sort_by;
+    pag.order = pag.order == undefined ? "asc" : pag.order;
+    
+    pag.limit = pag.limit == undefined ? 15 : (Number(pag.limit)) > 500 ? 500 : (Number(pag.limit));
+    pag.page = pag.page == undefined ? 0 : pag.page;
 
-        const skip = find_offset(validated_pag.limit, validated_pag.page);
+    const skip: number = find_offset(pag.limit, (Number(pag.page)));
 
-        try {
-            req.pag_option = new PaginationOptions(skip, validated_pag.limit, validated_pag.sort_by, validated_pag.order);
-            next();
-        } catch (error) {
-            next(error);
-        }
-    })
-    .catch((err) => {
-        let exc: Exception = new ValidationExc(err);
-        next(exc);
-    })
+    try {
+        req.pag_option = new PaginationOptions(skip, pag.limit, pag.sort_by, pag.order);
+        next();
+    } catch (error) {
+        next(error);
+    }
 }
 
 export default pagination_middleware;
