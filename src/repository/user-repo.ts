481ea89ca36d172bbce5dfb91705baa7d2d538d3
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-08-02 15:13:05 +0300

    updated timezone and format of date

--- src/repository/user-repo.ts
+++ src/repository/user-repo.ts
@@ -2,28 +2,28 @@ import { User } from "../model/user";
 import { v4 as uuid } from 'uuid';
 import { DBExc, UserNotFoundExc } from "../common/exception";
 import db from "../db/db";
-import format_date from "../common/date-formatter";
 import { FilterUser } from "../interface/i_filter";
 import { UserList } from "../interface/i-user-list";
 import utils from "../common/utils";
 import { PaginationOptions } from "../common/pagination-options";
 import { UserLogin } from "../model/user-login";
+import moment from "moment-timezone";
 
 export class UserRepo {
     
     async add_new_user(user: User): Promise<User> {
         return new Promise(async (resolve, reject) => {
             user.user_id = uuid();
-            user.signup_date = format_date(new Date);
-            console.log("date", user);
-            console.log(new Date())
+            user.signup_date = moment().tz("Europe/Istanbul");
             
             await db.knx<User>("user")
             .insert(user)
-            .returning("*")// edit this!!
+            .returning(["user_id", "id", "name", "surname", "send_ads", "email", "signup_date"])
             .then((new_user) => {
                 if (new_user[0]) {
-                    resolve(new_user[0]); //add new user
+                    console.log(new_user[0])
+                    new_user[0].signup_date = moment(new_user[0].signup_date).tz("Europe/Istanbul").format("DD.MM.YYYY HH:mm"); //service?
+                    resolve(new_user[0] as User);
                 } else {
                     reject(new DBExc("new user cannot be added")); 
                 }                
@@ -38,10 +38,12 @@ export class UserRepo {
     async get_user(user_id: string): Promise<User> {
         return new Promise<User> (async (resolve, reject) => {
             await db.knx("user")
-            .select("*")
+            .select("user_id", "id", "name", "surname", "send_ads", "email", "signup_date")
             .where("user_id", user_id)
             .then((result) => {
                 if (result[0]) {
+                    console.log(result[0].signup_date)
+                    result[0].signup_date = moment(result[0].signup_date).tz("Europe/Istanbul").format("DD.MM.YYYY HH:mm");
                     resolve(result[0]);
                 } else {
                     reject(new UserNotFoundExc()); 
@@ -60,7 +62,8 @@ export class UserRepo {
             .select("user_id", "email", "password")
             .where("email", email)
             .then((result) => {
-                if (result[0]) {
+                if (result[0]) {                    
+                    result[0].signup_date = moment(result[0].signup_date).tz("Europe/Istanbul").format("DD.MM.YYYY HH:mm");                    
                     resolve(result[0]);
                 } else {
                     reject(new UserNotFoundExc()); 
@@ -122,7 +125,7 @@ export class UserRepo {
         });
     }
 
-    async get_users(filter: FilterUser, options: PaginationOptions): Promise<UserList> { //?
+    async get_users(filter: FilterUser, options: PaginationOptions): Promise<UserList> {
         return new Promise<UserList> (async (resolve, reject) => {
             await db.knx("user") //count filtered users
             .count("id as count")
@@ -131,7 +134,7 @@ export class UserRepo {
             })
             .then((count_result) => {
                 db.knx("user") //select users
-                .select("*")
+                .select("user_id", "id", "name", "surname", "send_ads", "email", "signup_date")
                 .limit(options.limit as number)
                 .offset(options.skip)
                 .orderBy(options.sort_by, options.order || "asc")
@@ -139,6 +142,9 @@ export class UserRepo {
                     utils.user_list_builder(query_builder, filter);
                 })
                 .then((result) => {
+                    for (let user of result) { //?
+                        user.signup_date = moment(user.signup_date).tz("Europe/Istanbul").format("DD.MM.YYYY HH:mm");
+                    }
                     const data: UserList = { total_count: count_result[0].count as number, users: result};
                     resolve(data);                                    
                 })
