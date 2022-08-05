66d4f7a70aa1a96089a069fe8d49affb4963136e
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-08-05 09:30:43 +0300

    updated auth-middleware

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -5,21 +5,26 @@ import { UnauthExc } from '../common/exception';
 import config from "../config/config";
 
 function auth_middleware(req: ReqAuth, res: Response, next: NextFunction) {
-    const token = req.headers.authorization?.split(" ")[1];
+    let token: string;
+    if (req.headers.authorization?.length == 2) {
+        token = req.headers.authorization?.split(" ")[1];
 
-    if (token) {
-        jwt.verify(token, config.TOKEN_SECRET, (error, decoded) => {
-            if (error) {
-                next(error);
-            } else {
-                try {
-                    req.auth_decoded = decoded;
-                    next();
-                } catch (error) {
+        if (token) {
+            jwt.verify(token, config.TOKEN_SECRET, (error, decoded) => {
+                if (error) {
                     next(error);
+                } else {
+                    try {
+                        req.auth_decoded = decoded;
+                        next();
+                    } catch (error) {
+                        next(error);
+                    }
                 }
-            }
-        }); 
+            }); 
+        } else {
+            next(new UnauthExc());
+        }
     } else {
         next(new UnauthExc());
     }
