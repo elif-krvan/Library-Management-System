ca62a38a0470d78597d15c54e2b9cd1288b324be
Elif <elif.kervan@ug.bilkent.edu.tr>
2022-08-10 10:38:01 +0300

    added role repository

---
+++ src/repository/role-repo.ts
@@ -0,0 +1,63 @@
+import { DBExc, UserNotFoundExc } from "../common/exception";
+import db from "../db/db";
+import { IRole } from "../interface/i-role";
+
+export class RoleRepo {
+    
+    async add_role(user: IRole): Promise<boolean> {
+        return new Promise<boolean> (async (resolve, reject) => {
+            
+            await db.knx("user_role")
+            .insert(user)
+            .returning("id")
+            .then((new_role) => {
+                if (new_role[0]) {
+                    console.log("role reppo", new_role);
+                    resolve(true);
+                } else {
+                    reject(new DBExc("new role cannot be added")); 
+                }                
+            })
+            .catch((err) => {
+                reject(new DBExc(err));
+            });            
+        });        
+    }
+
+    async get_user_roles(user_id: string): Promise<any> { //fix this
+        return new Promise<any> (async (resolve, reject) => {
+            await db.knx("user_role")
+            .select("role")
+            .where("user_id", user_id)
+            .then((result) => {
+                if (result[0]) {
+                    console.log("get roles", result[0]);
+                    resolve(result[0]);
+                } else {
+                    reject(new UserNotFoundExc()); 
+                }  
+            })
+            .catch((err) => {
+                reject(new DBExc(err));
+            }); 
+        });      
+    }
+
+    async delete_user(user_id: string): Promise<boolean> {
+        return new Promise<boolean> (async (resolve, reject) => {
+            await db.knx("user_role")
+            .where("user_id", user_id)
+            .del()
+            .then((result) => {
+                if (result != 0) {
+                    resolve(true);
+                } else {
+                    reject(new UserNotFoundExc()); 
+                }
+            })
+            .catch((err) => {
+                reject(new DBExc(err));
+            });            
+        });
+    }
+}
